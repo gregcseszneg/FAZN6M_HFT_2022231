ae81904bc326eb440fc3f500474bbaf9a666fc0d
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-10-31 01:51:52 +0100

    Models - RecordLabel: Attributes are added

--- FAZN6M_HFT_2022231.Models/RecordLabel.cs
+++ FAZN6M_HFT_2022231.Models/RecordLabel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace FAZN6M_HFT_2022231.Models
 {
     internal class RecordLabel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RecordLabelId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime YearOfFoundation { get; set; }
+
+        public string Country { get; set; }
+        public string Headquarters { get; set; }
     }
 }
