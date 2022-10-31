6500f3a9366da5500ae39b8e757a2de30c15d0eb
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-10-31 01:49:35 +0100

    Models - Track is filled with attributes

--- FAZN6M_HFT_2022231.Models/Track.cs
+++ FAZN6M_HFT_2022231.Models/Track.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace FAZN6M_HFT_2022231.Models
 {
     internal class Track
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TrackId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Length { get; set; } //in sec
+
+        public string Genre { get; set; }
+
+        [Required]
+        public int MusicianId { get; set; }
+
+        public int AlbumId { get; set; }
     }
 }
