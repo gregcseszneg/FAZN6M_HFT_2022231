5f8c47883367ee493d711328137ff0b4d8c5bc12
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-10-31 01:57:35 +0100

    Tables are added

--- FAZN6M_HFT_2022231.Repository/MusicDbContext.cs
+++ FAZN6M_HFT_2022231.Repository/MusicDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Arch.EntityFrameworkCore;
+using FAZN6M_HFT_2022231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace FAZN6M_HFT_2022231.Repository
 {
-    internal class MusicDbContext
+    internal class MusicDbContext : DbContext
     {
+        public DbSet<Musician> musicians { get; set; }
+        public DbSet<Album> albums { get; set; }
+        public DbSet<Track> tracks { get; set; }
+        public DbSet<RecordLabel> recordlabels { get; set; }
 
+        
     }
 }
