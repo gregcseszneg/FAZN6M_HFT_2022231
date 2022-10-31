f29c7002d73ffafbaabe9e8d135393075614458c
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-10-31 01:40:07 +0100

    Models - Album is filled with attributes

--- FAZN6M_HFT_2022231.Models/Album.cs
+++ FAZN6M_HFT_2022231.Models/Album.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace FAZN6M_HFT_2022231.Models
 {
     internal class Album
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AlbumId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime DateOfRelease { get; set; }
+
+        [ForeignKey(nameof(Musician))]
+        public int MusicianId { get; set; }
+
+        [ForeignKey(nameof(Track))]
+        public int TrackId { get; set; }
     }
 }
