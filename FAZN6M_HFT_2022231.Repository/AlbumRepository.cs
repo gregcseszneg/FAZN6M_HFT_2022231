752594bcfd2473d7448f0fb9c01d635d03bed7f0
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-17 00:31:45 +0100

    AlbumRepository is created, Read and Update methods are written

---
+++ FAZN6M_HFT_2022231.Repository/AlbumRepository.cs
@@ -0,0 +1,31 @@
+﻿using FAZN6M_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Repository
+{
+    internal class AlbumRepository : Repository<Album>, IRepository<Album>
+    {
+        public AlbumRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Album Read(int id)
+        {
+            return ctx.Albums.FirstOrDefault(x => x.AlbumId == id);
+        }
+
+        public override void Update(Album item)
+        {
+            var old = Read(item.AlbumId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
