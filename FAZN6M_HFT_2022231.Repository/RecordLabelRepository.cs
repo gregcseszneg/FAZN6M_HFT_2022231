a38b9dfd758b6943a8098b5e38d373bd75bcc68a
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-17 00:38:30 +0100

    RecordLabelRepository is cretaed, Read and Update methods are written

---
+++ FAZN6M_HFT_2022231.Repository/RecordLabelRepository.cs
@@ -0,0 +1,31 @@
+﻿using FAZN6M_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Repository
+{
+    internal class RecordLabelRepository : Repository<RecordLabel>, IRepository<RecordLabel>
+    {
+        public RecordLabelRepository(MusicDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override RecordLabel Read(int id)
+        {
+            return ctx.RecordLabels.FirstOrDefault(x => x.RecordLabelId == id);
+        }
+
+        public override void Update(RecordLabel item)
+        {
+            var old = Read(item.RecordLabelId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
