a33cf1f23f8b0e46eefc4486e49bc1e98e76c48c
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-22 16:50:31 +0100

    RecordLabelLogic class is created, crud methods are written

---
+++ FAZN6M_HFT_2022231.Logic/RecordLabelLogic.cs
@@ -0,0 +1,44 @@
+﻿using FAZN6M_HFT_2022231.Models;
+using FAZN6M_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Logic
+{
+    internal class RecordLabelLogic
+    {
+        IRepository<RecordLabel> repository;
+        public RecordLabelLogic(IRepository<RecordLabel> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(RecordLabel item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public RecordLabel Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<RecordLabel> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(RecordLabel item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
