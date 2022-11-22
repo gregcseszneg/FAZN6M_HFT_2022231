0b9e66e543a308860e8213e6e2ee3343740d7ac6
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-22 16:50:15 +0100

    MusicianLogic class is created, crud methods are written

---
+++ FAZN6M_HFT_2022231.Logic/MusicianLogic.cs
@@ -0,0 +1,42 @@
+﻿using FAZN6M_HFT_2022231.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using FAZN6M_HFT_2022231.Repository;
+using System.Linq;
+
+namespace FAZN6M_HFT_2022231.Logic
+{
+    
+    public class MusicianLogic
+    {
+        IRepository<Musician> repo;
+        public MusicianLogic(IRepository<Musician> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Musician item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Musician Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Musician> ReadAll()
+        {
+           return  this.repo.ReadAll();
+        }
+
+        public void Update(Musician item)
+        {
+            this.repo.Create(item);
+        }
+    }
+}
