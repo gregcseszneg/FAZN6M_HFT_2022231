c848b9f724c57026c230427dc344b90b14f85475
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-22 16:50:03 +0100

    AlbumLogic class is created, crud methods are written

---
+++ FAZN6M_HFT_2022231.Logic/AlbumLogic.cs
@@ -0,0 +1,43 @@
+﻿using FAZN6M_HFT_2022231.Models;
+using FAZN6M_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Logic
+{
+    public class AlbumLogic
+    {
+        IRepository<Album> repo;
+        public AlbumLogic(IRepository<Album> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Album item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Album Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Album> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Album item)
+        {
+            this.repo.Create(item);
+        }
+    }
+}
