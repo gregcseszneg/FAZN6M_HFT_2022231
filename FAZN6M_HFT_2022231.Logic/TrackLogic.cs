096e1ed553ab260f99331cce1831682c2dd724c7
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-22 16:50:42 +0100

    trackLogic class is created, crud methods are written

---
+++ FAZN6M_HFT_2022231.Logic/TrackLogic.cs
@@ -0,0 +1,44 @@
+﻿using FAZN6M_HFT_2022231.Models;
+using FAZN6M_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Logic
+{
+    internal class TrackLogic
+    {
+        IRepository<Track> repo;
+        public TrackLogic(IRepository<Track> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Track item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Track Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Track> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Track item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
