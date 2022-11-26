a9a8482d89b8978c94363123613f434805fc36ae
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-26 17:23:11 +0100

    Mock repositories are created for tracks and recordlabels as well

--- FAZN6M_HFT_2022231.Test/MusicianTester.cs
+++ FAZN6M_HFT_2022231.Test/MusicianTester.cs
@@ -12,22 +12,55 @@ namespace FAZN6M_HFT_2022231.Test
     [TestFixture]
     public class MusicianTester
     {
-        MusicianLogic logic;
+        MusicianLogic mLogic;
+        TrackLogic tLogic;
+        RecordLabelLogic rLogic;
+
         Mock<IRepository<Musician>> mockMusicianRepo;
+        Mock<IRepository<Track>> mockTrackRepo;
+        Mock<IRepository<RecordLabel>> mockRecordLabelRepo;
         [SetUp]
         public void InIt()
         {
-            mockMusicianRepo = new Mock<IRepository<Musician>>();
-            mockMusicianRepo.Setup(m => m.ReadAll()).Returns(new List<Musician>()
+            var musicians = new List<Musician>()
             {
                 new Musician("1#Pop Smoke#07/20/1999#New York City#USA#male#2"),
                 new Musician("2#Charlie Puth#12/02/1991#Rumson#USA#male#1"),
                 new Musician("3#Tyga#11/19/1998#Compton#USA#male#3"),
                 new Musician("4#Travis Scott#04/30/1991#Houston#USA#male#3"),
-                new Musician("5#Joyner Lucas#08/17/1988#Worcester#USA#male#")
-            }.AsQueryable());
+                new Musician("5#Russ#09/26/1992#Atlanta#USA#male#")
+            }.AsQueryable();
 
-            logic = new MusicianLogic(mockMusicianRepo.Object);
+            var tracks = new List<Track>()
+            {
+                new Track("1#Are You Entertained#156#5#"),
+                new Track("2#CIVIL WAR#144#5#1"),
+                new Track("3#GUESS WHAT#206#5#1"),
+                new Track("4#Dior#216#1#"),
+                new Track("5#See You Again#229#2#"),
+                new Track("6#Chosen#161#3#"),
+                new Track("7#HIGHEST IN THE ROOM#175#4#"),
+            }.AsQueryable();
+
+            var recordlabels = new List<RecordLabel>()
+            {
+                new RecordLabel("1#Capitol Records#1942#USA#Hollywood"),
+                new RecordLabel("2#RCA Records#1900#USA#New York City"),
+                new RecordLabel("3#Death Row Records#1991#USA#Beverly Hills")
+            }.AsQueryable();
+
+            mockMusicianRepo = new Mock<IRepository<Musician>>();
+            mockMusicianRepo.Setup(m => m.ReadAll()).Returns(musicians);
+
+            mockTrackRepo = new Mock<IRepository<Track>>();
+            mockTrackRepo.Setup(m => m.ReadAll()).Returns(tracks);
+
+            mockRecordLabelRepo = new Mock<IRepository<RecordLabel>>();
+            mockRecordLabelRepo.Setup(m => m.ReadAll()).Returns(recordlabels);
+
+            mLogic = new MusicianLogic(mockMusicianRepo.Object);
+            tLogic = new TrackLogic(mockTrackRepo.Object);
+            rLogic = new RecordLabelLogic(mockRecordLabelRepo.Object);
         }
         [Test]
         public void CheckCreate()
@@ -36,7 +69,7 @@ namespace FAZN6M_HFT_2022231.Test
             Musician newMusician = new Musician("6#Juice Wrld#12/02/1998#Chicago#USA#male#");
         
             //ACT
-            logic.Create(newMusician);
+            mLogic.Create(newMusician);
 
             //ASSERT
             mockMusicianRepo.Verify(x => x.Create(newMusician), Times.Once);
@@ -49,7 +82,7 @@ namespace FAZN6M_HFT_2022231.Test
             Musician newMusician = new Musician("6##12/02/1998#Chicago#USA#male#");
 
             //ASSERT
-            Assert.That(() => logic.Create(newMusician), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => mLogic.Create(newMusician), Throws.TypeOf<ArgumentException>());
         }
 
         [Test]
@@ -59,14 +92,14 @@ namespace FAZN6M_HFT_2022231.Test
             Musician newMusician = new Musician("6#Juice Wrld#12/02/1888#Chicago#USA#male#");
 
             //ASSERT
-            Assert.That(() => logic.Create(newMusician), Throws.TypeOf<ArgumentException>());
+            Assert.That(() => mLogic.Create(newMusician), Throws.TypeOf<ArgumentException>());
         }
 
         [Test]
         public void CheckDelete()
         {
             //ACT
-            logic.Delete(1);
+            mLogic.Delete(1);
 
             //ASSERT
             mockMusicianRepo.Verify(x => x.Delete(It.IsAny<int>()), Times.Once);
@@ -78,7 +111,7 @@ namespace FAZN6M_HFT_2022231.Test
             Musician newTravis =new Musician("4#Travis Scott#04/29/1991#Budapest#Hungary#male#3");
 
             //ACT
-            logic.Update(newTravis);
+            mLogic.Update(newTravis);
 
             //ASSERT
             mockMusicianRepo.Verify(x => x.Update(newTravis), Times.Once);
@@ -91,7 +124,7 @@ namespace FAZN6M_HFT_2022231.Test
             mockMusicianRepo.Setup(m => m.Read(2)).Returns(tyga);
 
             //ACT
-            var result = logic.Read(2);
+            var result = mLogic.Read(2);
             
             //ASSERT
             Assert.That(result, Is.EqualTo(tyga));
@@ -106,11 +139,11 @@ namespace FAZN6M_HFT_2022231.Test
                 new Musician("2#Charlie Puth#12/02/1991#Rumson#USA#male#1"),
                 new Musician("3#Tyga#11/19/1998#Compton#USA#male#3"),
                 new Musician("4#Travis Scott#04/30/1991#Houston#USA#male#3"),
-                new Musician("5#Joyner Lucas#08/17/1988#Worcester#USA#male#")
+                new Musician("5#Russ#09/26/1992#Atlanta#USA#male#")
             }.AsEnumerable();
 
             //ACT
-            var result = logic.ReadAll();
+            var result = mLogic.ReadAll();
 
             //ASSERT
             Assert.That(result, Is.EqualTo(shouldbe));
