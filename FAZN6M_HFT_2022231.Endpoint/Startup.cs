7469b22dbd7b58315d22538b5675d45641382e5b
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-27 23:37:00 +0100

    Endpoint required nugget package added, startup is written, json are completed

--- FAZN6M_HFT_2022231.Endpoint/Startup.cs
+++ FAZN6M_HFT_2022231.Endpoint/Startup.cs
@@ -1,8 +1,16 @@
+using FAZN6M_HFT_2022231.Logic;
+using FAZN6M_HFT_2022231.Models;
+using FAZN6M_HFT_2022231.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +20,34 @@ namespace FAZN6M_HFT_2022231.Endpoint
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get;}
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<MusicDbContext>();
+
+            services.AddTransient<IRepository<Musician>, MusicianRepository>();
+            services.AddTransient<IRepository<Track>, TrackRepository>();
+            services.AddTransient<IRepository<Album>, AlbumRepository>();
+            services.AddTransient<IRepository<RecordLabel>, RecordLabelRepository>();
+
+            services.AddTransient<IMusicianLogic, MusicianLogic>();
+            services.AddTransient<ITrackLogic, TrackLogic>();
+            services.AddTransient<IAlbumLogic, AlbumLogic>();
+            services.AddTransient<IRecordLabelLogic, RecordLabelLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FAZN6M_HFT_2022231.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +56,28 @@ namespace FAZN6M_HFT_2022231.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FAZN6M_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                
             });
         }
     }
