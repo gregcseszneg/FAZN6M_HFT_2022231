e93b8792d5c189828cd6afa7dd9c76f1e85ec045
cseszneg.gergo <cseszneg.gergo09@gmail.com>
2022-11-27 11:45:58 +0100

    New model classes for queries where we seelct virtual

---
+++ FAZN6M_HFT_2022231.Models/AvgAgeInRecordLabel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Models
+{
+    public class AvgAgeInRecordLabel
+    {
+        public string RecordLabel { get; set; }
+        public double AvgAge { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null)
+            {
+                return false;
+            }
+            {
+                return (obj as AvgAgeInRecordLabel).RecordLabel== this.RecordLabel && (obj as AvgAgeInRecordLabel).AvgAge == this.AvgAge;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RecordLabel, AvgAge);
+        }
+    }
+}
---
+++ FAZN6M_HFT_2022231.Models/SumOfMusicLength.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAZN6M_HFT_2022231.Models
+{
+    public class SumOfMusicLength
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null)
+            {
+                return false;
+            }
+            else
+            {
+                return (obj as SumOfMusicLength).Name == this.Name && (obj as SumOfMusicLength).Length == this.Length;
+            }
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Length);
+        }
+    }
+}
