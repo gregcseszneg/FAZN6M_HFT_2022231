d04c5e4a347a707079ff501b9cac390f496fd28b
cseszneg.gergo09 <cseszneg.gergo09@gmail.com>
2023-05-12 23:12:55 +0200

    labels added to the wpf app with the attributes of the selected table

--- FAZN6M_HFT_2022231.WpfClient/MainWindow.xaml.cs
+++ FAZN6M_HFT_2022231.WpfClient/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using FAZN6M_HFT_2022231.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,60 +39,110 @@ namespace FAZN6M_HFT_2022231.WpfClient
 
         private void Selector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            DataContext = new MainWindowViewModel();
             if (Selector.SelectedItem.ToString() == "Musicians")
             {
+                DetailStackLabel.Children.Clear();
                 asd.Selected = new Musician();
                 TableList.ItemsSource = ((MainWindowViewModel)DataContext).Musicians;
 
+                var properties = typeof(Musician).GetProperties().Where(p => !Attribute.IsDefined(p, typeof(RequiredAttribute)));
+                var reqproperties = typeof(Musician).GetProperties().Where(p => Attribute.IsDefined(p, typeof(RequiredAttribute)));
+
+                foreach (var prop in reqproperties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + " (Required):";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
+
+                foreach (var prop in properties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + ":";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
 
             }
             else if (Selector.SelectedItem.ToString() == "Albums")
             {
+                DetailStackLabel.Children.Clear();
                 asd.Selected = new Album();
                 TableList.ItemsSource = ((MainWindowViewModel)DataContext).Albums;
+
+                var properties = typeof(Album).GetProperties().Where(p => !Attribute.IsDefined(p, typeof(RequiredAttribute)));
+                var reqproperties = typeof(Album).GetProperties().Where(p => Attribute.IsDefined(p, typeof(RequiredAttribute)));
+
+                foreach (var prop in reqproperties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + " (Required):";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
+
+                foreach (var prop in properties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + ":";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
 
             }
             else if (Selector.SelectedItem.ToString() == "Tracks")
             {
+                DetailStackLabel.Children.Clear();
                 asd.Selected = new Track();
                 TableList.ItemsSource = ((MainWindowViewModel)DataContext).Tracks;
+
+                var properties = typeof(Track).GetProperties().Where(p => !Attribute.IsDefined(p, typeof(RequiredAttribute)));
+                var reqproperties = typeof(Track).GetProperties().Where(p => Attribute.IsDefined(p, typeof(RequiredAttribute)));
+
+                foreach (var prop in reqproperties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + " (Required):";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
+
+                foreach (var prop in properties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + ":";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
 
             }
             else if (Selector.SelectedItem.ToString() == "Record Labels")
             {
+                DetailStackLabel.Children.Clear();
                 asd.Selected = new RecordLabel();
                 TableList.ItemsSource = ((MainWindowViewModel)DataContext).RecordLabels;
+
+                var properties = typeof(RecordLabel).GetProperties().Where(p => !Attribute.IsDefined(p, typeof(RequiredAttribute)));
+                var reqproperties = typeof(RecordLabel).GetProperties().Where(p => Attribute.IsDefined(p, typeof(RequiredAttribute)));
+
+                foreach (var prop in reqproperties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + " (Required):";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
+
+                foreach (var prop in properties)
+                {
+                    Label lb = new Label();
+                    lb.Content = prop.Name + ":";
+                    DetailStackLabel.Children.Add(lb);
+
+                }
             }
         }
 
-        private void TableList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //if (Selector.SelectedItem is Musician)
-            //{
-            //    tb.Text = (asd.Selected as Musician).Name;
-            //    TableList.ItemsSource = asd.Musicians;
-
-            //}
-            //else if (Selector.SelectedItem is Album)
-            //{
-            //    tb.Text = (asd.Selected as Album).Name;
-            //    TableList.ItemsSource = asd.Albums;
-
-            //}
-            //else if (Selector.SelectedItem is Track)
-            //{
-
-            //    tb.Text = (asd.Selected as Track).Name;
-            //    TableList.ItemsSource = asd.Tracks;
-
-            //}
-            //else if (Selector.SelectedItem is RecordLabel)
-            //{
-
-            //    tb.Text = (asd.Selected as RecordLabel).Name;
-            //    TableList.ItemsSource = asd.RecordLabels;
-            //}
-        }
     }
 }
